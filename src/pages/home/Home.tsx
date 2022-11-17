51d87d28ee11bbf0f8a9990fc6a21e012e1fea55
Денис Колесников <d.kolesnikov@sbereducation.ru>
2022-11-18 00:36:07 +0500

    chore: update eslint config

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,14 +1,10 @@
-import { allCategories } from "../../entities/categories/model";
-import { useAppSelector } from "../../shared/config/store";
-import { useAsyncModel } from "../../shared/lib/useAysyncModel";
 import { Background } from "../../shared/ui/components/layout";
 import { Checkout } from "../../widgets/checkout/checkout";
 import { DishList } from "../../widgets/dishlist/dislist";
 import Header from "../../widgets/header/ui/header";
 import { ListTitle } from "../../widgets/listtitle/listtitle";
-import { Navbar } from "../../widgets/navbar/ui/navbar";
 
-function Home() {
+const Home = () => {
   return (
     <Background>
       <Header />
@@ -17,6 +13,6 @@ function Home() {
       <Checkout />
     </Background>
   );
-}
+};
 
 export default Home;
